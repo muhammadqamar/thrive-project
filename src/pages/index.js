e28ca6ef0cc894b73df2c51d5ae90b6c07ad7fe6
muhammadqamar <muhammadqamar111@gmail.com>
2022-11-25 12:30:07 +0500

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import styles from "../assets/styles/Home.module.scss";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <h1>New Next Project</h1>
+    </div>
+  );
+}
